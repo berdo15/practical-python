f518a74a7a8d2397efdaeaed89aeb2527647dc95
dberg <dominic.berg@itzbund.de>
2024-04-09 17:11:40 +0200

    First changes

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounce_height = 3/5
+bounce_number = 0
+
+while bounce_number < 10:
+    height = height*bounce_height
+    print(round(height, 4))
+    bounce_number += 1
