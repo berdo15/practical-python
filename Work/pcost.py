483aede014e2a4ff310f7632e077db746e33fd30
dberg <dominic.berg@itzbund.de>
2024-04-11 15:35:34 +0200

    11.04.2024

--- Work/pcost.py
+++ Work/pcost.py
@@ -9,10 +9,18 @@ def portfolio_cost(filename):
     f = open(filename)
     rows = csv.reader(f)
     total_cost = 0
-    next(rows) # skip header
-    for row in rows:
-        total_cost += float(row[1]) * float(row[2])
+    #next(rows) # skip header
+    headers = next(rows)
+    for rowno, row in enumerate(rows, start=1):
+        record = dict(zip(headers, row))
+        try:
+            nshare = int(record['shares'])
+            nprice = float(record['price'])
+            total_cost += nshare * nprice
+        except ValueError:
+            print(f'Row {rowno}: Bad row: {row}')
     f.close()
+    print(record)
     return(total_cost)
 
 if len(sys.argv) == 2:
--- Work/report.py
+++ Work/report.py
@@ -6,22 +6,31 @@ import pprint
 
 def read_portfolio(filename):
     '''
-    Turning portfolio.csv into dictionary (or tuple if uncommenting lines 17+18 and commenting lines 19-22)
+    Turning portfolio.csv into list of dictionaries (or tuple if uncommenting lines 17+18 and commenting lines 19-22)
     '''
     portfolio = []
     f = open(filename, 'r')
     rows = csv.reader(f)
-    next(rows)
-    
-    for row in rows:
+    headers = next(rows)
+
+    for rowno, row in enumerate(rows, start=1):
 #        rowt = (row[0], int(row[1]), float(row[2]))
 #        portfolio.append(rowt)
-        rowd = {}
-        rowd['name'] = row[0]
-        rowd['shares'] = int(row[1])
-        rowd['price'] = float(row[2])
-        portfolio.append(rowd)
-    
+#        rowd = {}
+#        rowd['name'] = row[0]
+#        rowd['shares'] = int(row[1])
+#        rowd['price'] = float(row[2])
+        report = dict(zip(headers, row))
+        reportd = {}
+        try:
+            reportd['name'] = report['name']
+            reportd['shares'] = int(report['shares'])
+            reportd['price'] = float(report['price'])
+
+        except ValueError:
+            print(f'Row {rowno}: Bad row: {row}')
+        portfolio.append(reportd)
+
     return(portfolio)
 
 
@@ -32,7 +41,6 @@ def read_prices(filename):
     prices = {}
     f = open(filename, 'r')
     rows = csv.reader(f)
-    next(rows)
     for row in rows:
         if len(row) == 0:
             continue
@@ -40,22 +48,22 @@ def read_prices(filename):
     return(prices)
 
 
-def gains_losses(price, value):
+def gains_losses(portfolio, price):
     '''
     Calculating the gains/losses when comparing the portfolio.csv and prices.csv files    
     '''
     current_value = 0
     total_cost = 0
-    for v in value:
-        total_cost += v['shares'] * v['price']
-        if v['name'] in price:
-            current_value += price[v['name']] * v['shares']
+    for c in portfolio:
+        total_cost += int(c['shares']) * float(c['price'])
+        if c['name'] in price:
+            current_value += price[c['name']] * int(c['shares'])
     gain_loss = current_value - total_cost
     return(gain_loss)
 
 portfolio = 'Data/portfolio.csv'
 prices = 'Data/prices.csv'
-retire = gains_losses(read_prices(prices), read_portfolio(portfolio))
+retire = gains_losses(read_portfolio(portfolio), read_prices(prices))
 print(f'Der Gewinn beträgt: €{retire:0.2f}')
 
 #price = read_prices(prices)
